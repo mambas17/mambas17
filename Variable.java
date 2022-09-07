c4dcb2560ea113721b6cf013c86685aff0366dcc
mambas27_ <112999615+mambas17@users.noreply.github.com>
2022-09-07 04:41:45 +0000

    isi data

---
+++ Variable.java
@@ -0,0 +1,25 @@
+class Variable{
+
+  public static void main(String[] args) {
+
+    String nama = " Aco Rizky Aditya";
+      
+    int angka_1 = 17;
+
+    String angka2 = "3";
+
+    System.out.println("Selamat pagi" +nama);
+
+    System.out.println("Welcome To My Profile, Umur: ");
+    System.out.println(3+angka_1);
+
+     System.out.println("Operasi MTK di dalam Integer");
+    System.out.println(3+angka_1);
+
+    System.out.println("Operasi MTK di dalam String");
+    System.out.println(3+angka2);
+    
+
+      
+  }
+}
